cfc792a2fc5a5c0fe847c3038570de15c8399340
rodionmatytsin <79132260269@rambler.ru>
2025-04-27 16:24:09 +0700

    fix

--- main/schemas/achievements.py
+++ main/schemas/achievements.py
@@ -1,10 +1,20 @@
 from uuid import UUID
-from pydantic import BaseModel
+from fastapi import HTTPException
+from pydantic import BaseModel, field_validator
 from main.schemas.responses import DefaultResponse
 
 
 class AchievementAdd(BaseModel):
     description: str
+
+    @field_validator('description')
+    def check_description_(cls, description_):
+        if description_ is None or description_ == '':
+            raise HTTPException(
+                status_code=400,
+                detail={'result': False, 'message': 'Поле «Наименование достижения» должно быть не пустым!', 'data': {}}
+            )
+        return description_
 
 
 class AchievementRegular(BaseModel):
