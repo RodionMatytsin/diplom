6fd77e54476b394bb4abd8e3e4d148667eb60bf0
rodionmatytsin <79132260269@rambler.ru>
2025-05-04 02:10:29 +0700

    fix

--- main/views/views.py
+++ main/views/views.py
@@ -6,3 +6,8 @@ from fastapi.responses import HTMLResponse
 @main.get("/", response_class=HTMLResponse, status_code=200, tags=["Views"])
 async def index(request: Request):
     return templates.TemplateResponse("index.html", context={"request": request})
+
+
+@main.get("/schoolchildren", response_class=HTMLResponse, status_code=200, tags=["Views"])
+async def schoolchildren(request: Request):
+    return templates.TemplateResponse("schoolchildren.html", context={"request": request})
---
+++ static/js/schoolchildren.js
@@ -0,0 +1,55 @@
+const main = document.getElementById("main");
+const testing = document.getElementById("testing");
+const profile_settings = document.getElementById("profile_settings");
+const main__wrapper = document.getElementById("main__wrapper");
+const main__wrapper__list = document.getElementById("main__wrapper__list");
+const testing__wrapper = document.getElementById("testing__wrapper");
+const testing__wrapper__list = document.getElementById("testing__wrapper__list");
+const profile__settings__wrapper = document.getElementById("profile__settings__wrapper");
+const profile__settings__wrapper__list = document.getElementById("profile__settings__wrapper__list");
+
+main.addEventListener('click', () => {
+    main.classList.add("btn_active");
+    testing.classList.remove("btn_active");
+    profile_settings.classList.remove("btn_active"); // Исправлено
+    main__wrapper.classList.add("hiden");
+    main__wrapper__list.classList.add("hiden");
+    testing__wrapper.classList.remove("hiden");
+    testing__wrapper__list.classList.remove("hiden");
+    profile__settings__wrapper.classList.remove("hiden");
+    profile__settings__wrapper__list.classList.remove("hiden");
+    main__wrapper__list.innerHTML='';
+    testing__wrapper__list.innerHTML='';
+    profile__settings__wrapper__list.innerHTML='';
+})
+
+testing.addEventListener('click', () => {
+    main.classList.remove("btn_active");
+    testing.classList.add("btn_active");
+    profile_settings.classList.remove("btn_active"); // Исправлено
+    main__wrapper.classList.remove("hiden");
+    main__wrapper__list.classList.remove("hiden");
+    testing__wrapper.classList.add("hiden");
+    testing__wrapper__list.classList.add("hiden");
+    profile__settings__wrapper.classList.remove("hiden");
+    profile__settings__wrapper__list.classList.remove("hiden");
+    main__wrapper__list.innerHTML='';
+    testing__wrapper__list.innerHTML='';
+    profile__settings__wrapper__list.innerHTML='';
+})
+
+profile_settings.addEventListener('click', () => {
+    main.classList.remove("btn_active");
+    testing.classList.remove("btn_active");
+    profile_settings.classList.add("btn_active"); // Исправлено
+    main__wrapper.classList.remove("hiden");
+    main__wrapper__list.classList.remove("hiden");
+    testing__wrapper.classList.remove("hiden");
+    testing__wrapper__list.classList.remove("hiden");
+    profile__settings__wrapper.classList.add("hiden");
+    profile__settings__wrapper__list.classList.add("hiden");
+    main__wrapper__list.innerHTML='';
+    testing__wrapper__list.innerHTML='';
+    profile__settings__wrapper__list.innerHTML='';
+})
+
