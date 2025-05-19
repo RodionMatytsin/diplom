bd08e3cbf2b35f2b4de6700b35148e5dc2229858
rodionmatytsin <79132260269@rambler.ru>
2025-05-19 15:36:15 +0700

    fix front

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,8 +1,10 @@
 let login = document.getElementById('login'),
     password = document.getElementById('password'),
+    passwordRepeat = document.getElementById('passwordRepeat'),
     role = document.getElementById('role'),
     signupLogin = document.getElementById('signupLogin'),
     signupPassword = document.getElementById('signupPassword'),
+    signupPasswordRepeat = document.getElementById('signupPasswordRepeat'),
     phoneNumber = document.getElementById('phoneNumber'),
     fio = document.getElementById('fio'),
     day = document.getElementById('day'),
@@ -28,6 +30,12 @@ password.addEventListener('input', function() {
     }
 });
 
+passwordRepeat.addEventListener('input', function() {
+    if (!/^[a-zA-Z0-9_.\-!]+$/.test(passwordRepeat.value)) {
+        passwordRepeat.value = passwordRepeat.value.replace(/[^a-zA-Z0-9_.\-!]/g, '');
+    }
+});
+
 signupLogin.addEventListener('input', function() {
     if (!/^[a-zA-Z0-9_\-]+$/.test(signupLogin.value)) {
         signupLogin.value = signupLogin.value.replace(/[^a-zA-Z0-9_\-]/g, '');
@@ -40,55 +48,69 @@ signupPassword.addEventListener('input', function() {
     }
 });
 
+signupPasswordRepeat.addEventListener('input', function() {
+    if (!/^[a-zA-Z0-9_.\-!]+$/.test(signupPasswordRepeat.value)) {
+        signupPasswordRepeat.value = signupPasswordRepeat.value.replace(/[^a-zA-Z0-9_.\-!]/g, '');
+    }
+});
+
 function signup_user() {
-    sendRequest(
-        'POST',
-        '/api/signup',
-        true,
-        {
-            "phone_number": phoneNumber.value,
-            "fio": fio.value.trim(),
-            "birthday": (year.value+'-'+month.value+'-'+day.value),
-            "gender": gender.value,
-            "role": role.value,
-            "login": signupLogin.value.trim(),
-            "password": signupPassword.value.trim()
-        },
-        function (data) {
-            console.log(data);
-            if (data.data.is_teacher) {
-                window.location.href = "/teachers";
-            } else {
-                window.location.href = "/schoolchildren";
+    if (signupPassword.value !== signupPasswordRepeat.value) {
+        show_error('Пароли не совпадают!', 'Ошибка');
+    } else {
+        sendRequest(
+            'POST',
+            '/api/signup',
+            true,
+            {
+                "phone_number": phoneNumber.value,
+                "fio": fio.value.trim(),
+                "birthday": (year.value+'-'+month.value+'-'+day.value),
+                "gender": gender.value,
+                "role": role.value,
+                "login": signupLogin.value.trim(),
+                "password": signupPassword.value.trim()
+            },
+            function (data) {
+                console.log(data);
+                if (data.data.is_teacher) {
+                    window.location.href = "/teachers";
+                } else {
+                    window.location.href = "/schoolchildren";
+                }
+            },
+            function (data) {
+                console.log(data)
+                show_error(data.message, 'Ошибка');
             }
-        },
-        function (data) {
-            console.log(data)
-            show_error(data.message, 'Ошибка');
-        }
-    )
+        )
+    }
 }
 
 function login_user() {
-    sendRequest(
-        'POST',
-        '/api/login',
-        true,
-        {
-            "login": login.value.trim(),
-            "password": password.value.trim()
-        },
-        function (data) {
-            console.log(data);
-            if (data.data.is_teacher) {
-                window.location.href = "/teachers";
-            } else {
-                window.location.href = "/schoolchildren";
+    if (password.value !== passwordRepeat.value) {
+        show_error('Пароли не совпадают!', 'Ошибка');
+    } else {
+        sendRequest(
+            'POST',
+            '/api/login',
+            true,
+            {
+                "login": login.value.trim(),
+                "password": password.value.trim()
+            },
+            function (data) {
+                console.log(data);
+                if (data.data.is_teacher) {
+                    window.location.href = "/teachers";
+                } else {
+                    window.location.href = "/schoolchildren";
+                }
+            },
+            function (data) {
+                console.log(data)
+                show_error(data.message, 'Ошибка');
             }
-        },
-        function (data) {
-            console.log(data)
-            show_error(data.message, 'Ошибка');
-        }
-    )
+        )
+    }
 }
